f2d231639f963301319036c11ca03de4bee06798
Salman Prottoy <salman.prottoy@gmail.com>
2021-11-26 15:46:00 +0600

    project initialized

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//config
+app.set("view engine", "ejs");
+
+//route
+app.get("/", (req, res) => {
+  res.send("Hello from express server");
+});
+
+//server startup
+app.listen(3000, (error) => {
+  console.log("express server started at 3000...");
+});
